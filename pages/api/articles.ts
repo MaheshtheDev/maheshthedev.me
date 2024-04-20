fddb15183243d1434a0bfa93cc2def287458d5ab
MaheshtheDev <mahesh.svmr@gmail.com>
2024-04-19 23:43:47 -0700

    updated runtime experimental-edge

--- pages/api/articles.ts
+++ pages/api/articles.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getRecentArticles } from "../../lib/recent-articles";
 
-export const config = { runtime: "experimental-edge" };
+export const runtime = "experimental-edge";
 
 export default async function handler(
   req: NextApiRequest,
