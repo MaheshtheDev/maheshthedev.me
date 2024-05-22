a0680a17c40f51463baac4c72b350e70fa0aa1b2
MaheshtheDev <mahesh.svmr@gmail.com>
2024-05-21 23:18:08 -0700

    add: project card for building page

--- app/baking/page.tsx
+++ app/baking/page.tsx
@@ -1,5 +1,22 @@
 import Image from "next/image";
 
+const projects = [
+  {
+    name: "Splitrck",
+    description: "Analytics for Splitwise",
+    builtWith: ["React", "Next.js", "TailwindCSS"],
+    image:
+      "https://github.com/MaheshtheDev/Splitrck/blob/main/public/apple-touch-icon.png?raw=true",
+  },
+  {
+    name: "myFRS",
+    description: "retail store management system",
+    builtWith: ["React", "Next.js", "PWA"],
+    image:
+      "https://raw.githubusercontent.com/MaheshtheDev/myfrs-pwa/main/public/logo.svg",
+  },
+];
+
 export default function Baking() {
   return (
     <div>
@@ -14,7 +31,7 @@ export default function Baking() {
             alt="Your Name"
           />
           <div className="mx-3">
-            <p className="text-md">Mahesh Sanikommu</p>
+            <p className="text-md font-medium">Mahesh Sanikommu</p>
             <div className="opacity-60 text-sm">Full Stack Developer</div>
           </div>
         </div>
@@ -22,7 +39,42 @@ export default function Baking() {
           hire me
         </button>
       </header>
-      <h2 className="px-2 text-lg font-semibold">building, lately!</h2>
+      <h2 className="px-2 text-lg text-blue-400 font-semibold">
+        building, lately!
+      </h2>
+      <section className="mx-2 py-2 gap-4 flex flex-col">
+        {projects.map((project, index) => (
+          <div
+            className="px-3 py-2 bg-[#1B1818] font-medium rounded-md relative overflow-hidden"
+            key={index}
+          >
+            <Image
+              src={
+                project.image ||""
+              }
+              width={60}
+              height={60}
+              alt="Splitrck"
+              className="absolute -top-3 -right-2 rounded-full brightness-50"
+            />
+            <p>{project.name}</p>
+            <p className="text-[12px] opacity-75">{project.description}</p>
+            <div className="flex">
+              <p className="text-[10px] opacity-50">BUILT WITH: </p>
+              <div className="flex">
+                {project.builtWith.map((tech, index) => (
+                  <span
+                    key={index}
+                    className="text-[10px] px-1 bg-[#1F1717] rounded-full"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </section>
     </div>
   );
 }
